54194bd3a97d2fdf39ad859549690b8a3f044b56
Halil Beycan <50718965+BeycanDeveloper@users.noreply.github.com>
2023-10-11 22:15:50 +0800

    added randomUUID check

--- src/adapters/torus.js
+++ src/adapters/torus.js
@@ -1,5 +1,16 @@
 module.exports = (provider) => {
 
+    if (window.crypto && !window.crypto.randomUUID) {
+        window.crypto.randomUUID = () => {
+            let uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+                const r = (Math.random() * 16) | 0;
+                const v = c === 'x' ? r : (r & 0x3) | 0x8;
+                return v.toString(16);
+            });
+            return uuid;
+        };
+    }
+    
     const { TorusWalletAdapter } = require('@solana/wallet-adapter-torus');
     const wallet = new TorusWalletAdapter({ network: provider.network.node });
 
